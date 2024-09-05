c324286fbde769ba517b402bf731243b77cb1643
Jonathan Sundqvist <jonathan+git@argpar.se>
2024-09-05 19:24:55 +0200

    Add commented out feed for dev

--- pelicanconf.py
+++ pelicanconf.py
@@ -27,6 +27,7 @@ PLUGINS = [
 TIMEZONE = 'Europe/Berlin'
 
 # Feed generation is usually not desired when developing
+# FEED_ALL_ATOM = 'feeds/all.atom.xml'
 FEED_ALL_ATOM = None
 CATEGORY_FEED_ATOM = None
 TRANSLATION_FEED_ATOM = None
