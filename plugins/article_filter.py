ff0881581836e74792347be200e91c383d8091a7
Jonathan Sundqvist <jonathan+git@argpar.se>
2024-09-05 19:00:07 +0200

    Modify writer so that we can customize feeds a bit

--- plugins/article_filter.py
+++ plugins/article_filter.py
@@ -15,11 +15,57 @@ Possibly take inspiration from this.
 https://github.com/pelican-plugins/feed-filter
 """
 
+from urllib.parse import urlparse
+
+from jinja2 import Markup
 from pelican import signals
+from pelican.utils import set_date_tzinfo
 from pelican.writers import Writer
 
 
-class ExcludeWriter(Writer):
+def load_template(generator):
+    """Load 'feed' template and store its macros in CustomWriter."""
+    try:
+        tpl_macros = generator.get_template("feed").make_module(generator.context)
+        CustomWriter.macros = tpl_macros
+    except Exception:
+        pass
+
+
+class CustomWriter(Writer):
+    """
+    The functionality of this writer does two things.
+    - Allowing you to customize feeds using template macros.
+    - Filter out the "photography" category from index.html
+    """
+    macros = None  # Template macros
+
+    def _add_item_to_the_feed(self, feed, item):
+        if self.macros is None:
+            # Fall back to default behaviour if macros was not loaded
+            return Writer._add_item_to_the_feed(self, feed, item)
+
+        title = Markup(item.title).striptags()
+        link = '%s/%s' % (self.site_url, item.url)
+        content = item.get_content(self.site_url)
+
+        description = self.macros.description(item) if hasattr(self.macros, 'description') else content
+        feed.add_item(
+            title=self.macros.title(item) if hasattr(self.macros, 'title') else title,
+            link=self.macros.link(item).strip() if hasattr(self.macros, 'link') else link,
+            unique_id='tag:{},{}:{}'.format(
+                urlparse(link).netloc,
+                item.date.date(),
+                urlparse(link).path.lstrip('/')
+            ),
+            description=description,
+            categories=item.tags if hasattr(item, 'tags') else None,
+            author_name=getattr(item, 'author', ''),
+            pubdate=set_date_tzinfo(
+                item.modified if hasattr(item, 'modified') else item.date,
+                self.settings.get('TIMEZONE', None)
+            )
+        )
 
     def write_file(
         self,
@@ -53,9 +99,10 @@ class ExcludeWriter(Writer):
 
 
 def get_writer(sender):
-    return ExcludeWriter
+    return CustomWriter
 
 
 def register():
     """Register the new plugin"""
+    signals.article_generator_init.connect(load_template)
     signals.get_writer.connect(get_writer)
